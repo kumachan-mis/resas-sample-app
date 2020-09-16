d97fcd210a2a5e095719e551f11fe30d9b300a59
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-09-16 22:27:07 +0900

    feat: get population composition perYear in a prefecture

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,3 +2,13 @@ export interface Prefecture {
   prefCode: number;
   prefName: string;
 }
+
+export interface Population {
+  year: number;
+  value: number;
+}
+
+export interface PopulationTransition {
+  populations: Population[];
+  boundaryYear: number;
+}

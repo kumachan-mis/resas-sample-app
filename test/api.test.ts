56bc395be662181ca6822ed743e0c4879736bdf5
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-09-19 20:24:20 +0900

    test: add API test

---
+++ test/api.test.ts
@@ -0,0 +1,45 @@
+import fetchMock from "fetch-mock";
+import * as path from "path";
+import * as fs from "fs";
+
+import { ResasApi } from "../src/api";
+
+test("success to fetch prefecture list", async () => {
+  const rawFile = fs.readFileSync(
+    path.resolve(__dirname, "..", "test-fixtures", "raw-prefectures.json"),
+    { encoding: "utf-8" }
+  );
+  const expectedFile = fs.readFileSync(
+    path.resolve(__dirname, "..", "test-fixtures", "expected-prefectures.json"),
+    { encoding: "utf-8" }
+  );
+  const rawJson = JSON.parse(rawFile);
+  const expectedJson = JSON.parse(expectedFile);
+
+  fetchMock.get("https://opendata.resas-portal.go.jp/api/v1/prefectures", rawJson);
+  const prefectures = await ResasApi.fetchPrefectureList();
+  expect(JSON.stringify(prefectures)).toEqual(JSON.stringify(expectedJson));
+  fetchMock.restore();
+});
+
+test("success to fetch population transition", async () => {
+  const rawFile = fs.readFileSync(
+    path.resolve(__dirname, "..", "test-fixtures", "raw-population-transition.json"),
+    { encoding: "utf-8" }
+  );
+  const expectedFile = fs.readFileSync(
+    path.resolve(__dirname, "..", "test-fixtures", "expected-population-transition.json"),
+    { encoding: "utf-8" }
+  );
+  const rawJson = JSON.parse(rawFile);
+  const expectedJson = JSON.parse(expectedFile);
+
+  fetchMock.get(
+    "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
+    rawJson,
+    { query: { prefCode: 1, cityCode: "-" } }
+  );
+  const PopulationTransition = await ResasApi.fetchPopulationTransition(1);
+  expect(JSON.stringify(PopulationTransition)).toEqual(JSON.stringify(expectedJson));
+  fetchMock.restore();
+});
